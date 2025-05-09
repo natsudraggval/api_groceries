e0a0116229e8ffb5de5df801783ab9e903beaa6d
natusdraggval <natsudraggval@gmail.com>
2025-05-09 22:49:02 +0545

    fff image

---
+++ pages/test.js
@@ -0,0 +1,10 @@
+// pages/test.js
+export default function TestPage() {
+    return (
+      <div>
+        <h1>Test Image</h1>
+        <img src="/images/fanta.jpg" alt="Fanta" />
+      </div>
+    );
+  }
+  
